6b8bef3c082fbc9308b2c2fe21c350a11c833fe7
dichanger <dichanger@126.com>
2016-05-21 16:13:48 +0800

    fff

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 	$str = $_SERVER['HTTP_HOST'];
 	$str2 = $_SERVER["REQUEST_URI"]; 	
-	$str3 = $str.'/di'.$str2;
-	echo "<script>window.location.href=\"$str3\"</script>";
+	$str3 = 'http://'.$str.'/di'.$str2;
+	if($str2 == 'index.php'){	
+		echo "<script>window.location.href=\"$str3\"</script>";
+    }
