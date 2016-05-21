4931530d4eb0ee127b9f047f7e1a40a2366e2449
dichanger <dichanger@126.com>
2016-05-21 17:01:47 +0800

    1

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 	$str = $_SERVER['HTTP_HOST'];
 	$str2 = $_SERVER["REQUEST_URI"]; 	
-	$str3 = 'http://'.$str.'/di'.$str2;
-	if($str2 == 'index.php'){	
-		echo "<script>window.location.href=\"$str3\"</script>";
-    }
+	$p = "/(*.)/";
+	$r = "/di$1";
+	$str3 = preg_replace($p,$r,$str2);
+	
+	echo "<script>window.location.href=\"$str3\"</script>";
+    
