e81834629870907b23d4c3f7ff4523e3e86ba283
dichanger <1124337520@qq.com>
2016-05-25 17:01:57 +0800

    修改默认主页

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-	$str = $_SERVER['HTTP_HOST'];
-	$str2 = $_SERVER["REQUEST_URI"]; 	
-	$p = "/^(.*)/i";
-	$r = "/di$1";
-	$str3 = preg_replace($p,$r,$str2);
-	
-	echo "<script>window.location.href=\"$str3\"</script>";
+    echo "hello world";
+    phpinfo();
     
